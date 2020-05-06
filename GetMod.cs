d2fa8f06f836565b32fa48618e069da85855ea52
Paxygit <theonlypax@gmail.com>
2020-05-06 08:31:49 -0400

    Weapon Update
    Now randomly generates the name of the weapon, replacing the old TEMPWEAPON variable. Weapons done?

--- GetMod.cs
+++ GetMod.cs
@@ -9,16 +9,18 @@ namespace MagicItemGen
     {
         #region GAPI Static Declares
         static readonly string[] scopes = { SheetsService.Scope.Spreadsheets };
+        static SheetsService service;
         static readonly string appName = "Fixes";
         static readonly string spreadsheetID = "1g7hTUbNkulh-Z63PMEo1-lR4B0Kw-rKX9uC54iGSVFw";
         static readonly string sheetPrefix = "Prefix List";
         static readonly string sheetSuffix = "Suffix List";
-        static SheetsService service;
-        static readonly string tempweap = "TEMPWEAPON";
+        static readonly string sheetWeapType = "Weapon List";
+        static readonly string fifthEditionWeaponCPDM = "https://roll20.net/compendium/dnd5e/Weapons#content";
+        static private Random rand = new Random();
         #endregion
         public static string GetItem()
         {
-            Random rand = new Random();
+            
             GoogleCredential credential; // credential gen from json
 
             using (var stream = new FileStream("ItemGen-c89894405329.json", FileMode.Open, FileAccess.Read))
@@ -34,11 +36,54 @@ namespace MagicItemGen
             }); //run api service
 
              string[] prefix = ReadAffix(rand.Next(0, 19), 0);
-             string[] suffix = ReadAffix(rand.Next(0, 19), 1);
-            return prefix[0] + tempweap + suffix[0] + "\n\n" + 
+             string[] suffix = ReadAffix(rand.Next(0, 19), 1); 
+             string[] weaponType = GetWeaponType();
+
+            return prefix[0] + weaponType[0] + suffix[0] + "\n\n" + 
                 prefix[0] + ": " + prefix[1] + "\n\n" +
-                suffix[0] + ": " + suffix[1] + "\n\n";
+                suffix[0] + ": " + suffix[1] + "\n\n" +
+                weaponType[1];
         }
+
+        private static string[] GetWeaponType()
+        {
+            int weapArchetype = rand.Next(1, 4);
+            var range = "";
+            int n = 0;
+            switch (weapArchetype)
+            {
+                case 1: //SIMPLE MELEE
+                    range = $"{sheetWeapType}!A2:A11";
+                    n = 10;
+                    break;
+                case 2: //SIMPLE RANGED
+                    range = $"{sheetWeapType}!B2:B5";
+                    n = 4;
+                    break;
+                case 3: //MARTIAL MELEE
+                    range = $"{sheetWeapType}!C2:C19";
+                    n = 18;
+                    break;
+                case 4: //MARTIAL RANGED
+                    range = $"{sheetWeapType}!D2:D6";
+                    n = 5;
+                    break;
+            }
+
+            var request = service.Spreadsheets.Values.Get(spreadsheetID, range);
+            var values = request.Execute().Values; //USE FOR VALUES
+
+            switch (values.Count)
+            {
+                case 0:
+                    return new[] { "ERROR, UNABLE TO ACCESS ITEM TYPES" };
+
+                default:
+                    var value = values[rand.Next(0, n)]; // GEN THROUGH HERE
+                    return new[] { " " + value[0].ToString() + " ", fifthEditionWeaponCPDM };
+            }
+        }
+
         /// <summary>
         /// Pass your desired index into n, and 0 for suffix or 1 for prefix into suffixOrPrefix.
         /// </summary>
@@ -57,7 +102,6 @@ namespace MagicItemGen
                     range = $"{sheetSuffix}!A2:B21"; //FIRST COLUMN OF PREFIXES
                     break;
             }
-
             var request = service.Spreadsheets.Values.Get(spreadsheetID, range);
             var values = request.Execute().Values; //USE FOR VALUES
             var value = values[n]; // GEN THROUGH HERE
