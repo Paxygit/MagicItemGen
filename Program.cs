ffce46c1657e8633d3c5d1ad106afed6de0aa286
Paxygit <theonlypax@gmail.com>
2020-05-12 08:51:53 -0400

    Useful forced rarities
    Nothing special really. Added 100% rarity test numbers. Y'know, for testing.

--- Program.cs
+++ Program.cs
@@ -4,13 +4,22 @@ using System;
 // Feel free to fork and do your own stuff with it :)
 // Currently not in working order... relatable I guess
 // Licensed under GNU GPL 3.0
+
+/*USEFUL FORCES: 
+ * 500 - Rare 100%
+ * 250 - Magic 100%
+ * 100 - Normal 100% (why?)
+ */
 namespace MagicItemGen
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetMod.GetItem());
+            Console.WriteLine(GetMod.GetItem(500));
+            Console.WriteLine(GetMod.GetItem(500)); 
+            Console.WriteLine(GetMod.GetItem(500)); 
+            Console.WriteLine(GetMod.GetItem(500));
         }
     }
 }
