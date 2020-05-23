f2400f42c1f51619bf119b385736c31371843981
Paxygit <theonlypax@gmail.com>
2020-05-22 20:08:58 -0400

    The UI Update
    Added a UI so it can be used without the editor. It is temporary (I hope). Added all of the debug and nuget framework that wasn't included earlier for version control reasons.

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MagicItemGenUI
+{
+    // Paxydev
+    // Frank Kuenneke
+    // Feel free to fork and do your own stuff with it :)
+    // Currently not in working order... relatable I guess
+    // Licensed under GNU GPL 3.0
+    /*USEFUL FORCES: 
+     * 500 - Rare 100%
+     * 250 - Magic 100%
+     * 100 - Normal 100% (why?)
+     */
+    public partial class App : Application
+    {
+    }
+}
--- GetMod.cs
+++ GetMod.cs
@@ -1,10 +1,10 @@
-using Google.Apis.Auth.OAuth2;
+﻿using Google.Apis.Auth.OAuth2;
 using Google.Apis.Sheets.v4;
 using System;
 using System.Collections.Generic;
 using System.IO;
 
-namespace MagicItemGen
+namespace MagicItemGenUI
 {
     public class GetMod
     {
